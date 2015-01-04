13067228b4388047c1ab8d5d5d7ca25df4d7357e
Paretje <info@Online-Urbanus.be>
2015-01-04 18:59:46 +0100

    Use vnstat for bandwidth usage
    I changed this before the use of the git repo, but it seems like I
    forgot to add and commit then.

--- bin/getbandwidthusage.sh
+++ bin/getbandwidthusage.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 if [ -f /sys/class/net/ppp0/statistics/rx_bytes ] ; then
-	# echo aanvaardt standaard geen \n
-	# Opmerkelijk: bij dash wel? Wordt al geïnterpreteerd voor echo?
-	printf '+\np' | cat /sys/class/net/ppp0/statistics/rx_bytes /sys/class/net/ppp0/statistics/tx_bytes - | dc | /home/kevin/bin/bytes2human.sh
+	vnstat --oneline | sed 's/1;.*;\([^;]*\);[^;]*;[^;]*;[^;]*;[^;]*$/\1/'
 else
 	echo ""
 fi
