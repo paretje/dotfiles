8a6566bdabcea7a008371cf6d27a4aa182d21278
Paretje <info@Online-Urbanus.be>
2015-05-02 15:22:46 +0200

    Warn when monthly bandwidth is reached

--- bin/getbandwidthusage.sh
+++ bin/getbandwidthusage.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 if [ -f /sys/class/net/ppp0/statistics/rx_bytes ] ; then
-	vnstat --oneline | sed 's/1;.*;\([^;]*\);[^;]*;[^;]*;[^;]*;[^;]*$/\1/'
+	USAGE=$(vnstat --oneline | sed 's/1;.*;\([^;]*\);[^;]*;[^;]*;[^;]*;[^;]*$/\1/')
+	echo $USAGE
+
+	ORDER=$(echo $USAGE | cut -d ' ' -f2)
+	SIZE=$(echo $USAGE | cut -d '.' -f1)
+	if [ "$ORDER" = "MiB" -a $SIZE -gt 50 ]; then
+		notify-send 'Bandwidth used!'
+	fi
 else
 	echo ""
 fi
