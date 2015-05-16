249ca30d4f4a4a1d794ada4f47f769028d4db40f
Paretje <info@Online-Urbanus.be>
2015-05-16 17:36:02 +0200

    Delete Delhaize promo url fetcher, as obsolete

--- bin/getdelpromourl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-WEEK=$(date +%V)
-DAY=$(date +%u)
-if [ $DAY -gt 3 ]; then
-	DELWEEK=$(printf "%.2d" $(echo "$WEEK+1" | bc))
-else
-	DELWEEK="$WEEK"
-fi
-echo "http://nl.delhaize.be/-/media/Images/Promotions/Folders/DigiMag/2014/$DELWEEK/Promo-$DELWEEK-NL/download/magazine.pdf"
