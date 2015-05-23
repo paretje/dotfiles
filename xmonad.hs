459cd6ff9907d63b865ef0223963709300b72fbc
Paretje <info@Online-Urbanus.be>
2015-05-23 17:04:24 +0200

    Initial commit

---
+++ xmonad.hs
@@ -0,0 +1,20 @@
+import XMonad
+import XMonad.Config.Xfce
+import XMonad.Util.EZConfig
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.NoBorders
+
+main = xmonad $ xfceConfig
+	{ terminal = "xfce4-terminal"
+	, manageHook = myManageHook <+> manageHook xfceConfig
+	, layoutHook = smartBorders $ layoutHook xfceConfig
+	, focusFollowsMouse = False }
+	  `additionalKeysP`
+	[ ("M-S-z", spawn "xflock4") ]
+
+myManageHook = composeAll
+  [ className =? "Xfce4-notifyd" --> doIgnore
+  , className =? "Xfrun4" --> doFloat
+  , className =? "mpv" --> doFloat
+  , className =? "Wrapper" --> doFloat
+  , className =? "Pinentry" --> doIgnore ]
