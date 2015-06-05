2f33c62c07d76d61f1f50515a8bdaca9f337be3c
Paretje <info@Online-Urbanus.be>
2015-06-05 21:54:43 +0200

    Use fgrep to detect whether we're on GSM hotspot

--- bin/getbandwidthusage.sh
+++ bin/getbandwidthusage.sh
@@ -8,7 +8,7 @@ if [ -f /sys/class/net/ppp0/statistics/rx_bytes ] ; then
 	if [ "$ORDER" = "MiB" -a $SIZE -gt 50 ]; then
 		notify-send 'Bandwidth used!'
 	fi
-elif [ "$(/sbin/iwgetid)" = 'wlan0     ESSID:"LG-E410i_7795"' ]; then
+elif /sbin/iwgetid | fgrep 'LG-E410i_7795' > /dev/null; then
 	# echo differ between bash and dash: both require and support the option -n,
 	# but bash requires the option -e as well, to support the \n, while dash
 	# doesn't need it, and will simply output it. printf seems to be a more
