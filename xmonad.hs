150ef0cc95dc4579cf9516127f9666a2d7e33e76
Paretje <info@Online-Urbanus.be>
2015-07-29 09:16:16 +0200

    Cleanup of indentation xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -5,17 +5,17 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
 
 main = xmonad $ xfceConfig
-	{ modMask = mod4Mask
-        , terminal = "xfce4-terminal"
-	, manageHook = myManageHook <+> manageHook xfceConfig
-	, layoutHook = smartBorders $ layoutHook xfceConfig
-	, focusFollowsMouse = False }
-	  `additionalKeysP`
-	[ ("M-S-z", spawn "xflock4") ]
+    { modMask = mod4Mask
+    , terminal = "xfce4-terminal"
+    , manageHook = myManageHook <+> manageHook xfceConfig
+    , layoutHook = smartBorders $ layoutHook xfceConfig
+    , focusFollowsMouse = False }
+        `additionalKeysP`
+    [ ("M-S-z", spawn "xflock4") ]
 
 myManageHook = composeAll
-  [ className =? "Xfce4-notifyd" --> doIgnore
-  , className =? "Xfrun4" --> doFloat
-  , className =? "mpv" --> doFloat
-  , className =? "Wrapper" --> doFloat
-  , className =? "Pinentry" --> doIgnore ]
+    [ className =? "Xfce4-notifyd" --> doIgnore
+    , className =? "Xfrun4" --> doFloat
+    , className =? "mpv" --> doFloat
+    , className =? "Wrapper" --> doFloat
+    , className =? "Pinentry" --> doIgnore ]
