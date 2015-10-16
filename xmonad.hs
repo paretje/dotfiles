0129cba7daebdb9c68f8d5b9fbc64784ac20fd22
Paretje <info@Online-Urbanus.be>
2015-10-16 15:35:02 +0200

    Use pure xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,23 +1,36 @@
 import XMonad
-import XMonad.Config.Xfce
 import XMonad.Util.EZConfig
 import XMonad.Layout.NoBorders
 import qualified XMonad.StackSet as W
 import XMonad.Util.WindowProperties
 import Data.Monoid
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import System.Exit
 
 main :: IO ()
-main = xmonad $ xfceConfig
+main = xmonad =<< xmobar myConfig
+
+myConfig = defaultConfig
     { modMask = mod3Mask
-    , terminal = "xfce4-terminal"
-    , manageHook = hideNotifications <+> myManageHook <+> manageHook xfceConfig
-    , layoutHook = smartBorders $ layoutHook xfceConfig
+    , terminal = "exec xfce4-terminal -x sh -c 'TERM=xterm-256color exec tmux'"
+    , manageHook = hideNotifications <+> myManageHook <+> manageHook defaultConfig
+    , layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
     , focusFollowsMouse = False
     , clickJustFocuses = False }
         `additionalKeysP`
     [ ("M-S-z", spawn "xflock4")
     , ("M-p", spawn "exec gmrun")
-    , ("M-S-p", spawn "xfrun4") ]
+    , ("M-S-p", spawn "xfrun4")
+    , ("M-S-q", spawn "dmenu-logout")
+    , ("M-C-S-q", io (exitWith ExitSuccess))
+    , ("M-<F5>", spawn "xfce4-screenshooter -f -s cloud/screens")
+    , ("M-<F6>", spawn "if [ $(xbacklight | sed 's/\\..*$//') -ge 10 ] ; then xbacklight -dec 10 ; fi")
+    , ("M-<F7>", spawn "xbacklight -inc 10")
+    , ("M-<F8>", spawn "amixer set Master toggle")
+    , ("M-<F9>", spawn "amixer set Master 10%-")
+    , ("M-<F10>", spawn "amixer set Master 10%+")
+    ]
 
 myManageHook :: Query (Endo WindowSet)
 myManageHook = composeAll
