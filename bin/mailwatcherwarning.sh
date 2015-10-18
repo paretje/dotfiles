fcad18946dcaa5c1ffb7197ee5fa884a774a39cb
Paretje <info@Online-Urbanus.be>
2015-10-18 18:14:05 +0200

    Cleanup
     - addhdoutputmode.sh: integrated in xsession
     - batchmkv2ts: not exactly a recurring task
     - cbr2cbz: outdated?
     - cbz-ebook.sh: outdated
     - mailwatcherwarning.sh: no longer used
     - org2html: no longer used
     - post_dagelijksekost.sh: no longer used
     - psync: no longer needed
     - split-image.sh: hackish

--- bin/mailwatcherwarning.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-notify-send -i '/usr/share/icons/Tango/scalable/actions/mail-message-new.svg' 'Mail Ontvangen!' 'Kevin, u hebt zonet een e-mail ontvangen!\nOpen Icedove voor meer informatie.'
-
-beep -f 750 -r 3
-beep -f 300 -r 3
-beep -f 750 -r 3
