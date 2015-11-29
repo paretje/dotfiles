2efb6705e8fb30f45881e5eff9295cf29abc28a0
Paretje <info@Online-Urbanus.be>
2015-11-29 22:33:18 +0100

    Revert "Delete files first"
    This reverts commit 2dd9ad492d98e2d7f3bff90944e12a4dc0c146bf.

--- bin/syncrpi.sh
+++ bin/syncrpi.sh
@@ -3,6 +3,6 @@ ssh rpi "ls -1 /media/sda1-ata-WDC_WD10EADS-00M | sed '/^lost+found$/d'" | \
 while read dir; do
 	if [ -d "/storage/media/series/$dir" ]; then
 		echo "$dir"
-		lftp -e "mirror -R --delete-first /storage/media/series/\"$dir\" \"$dir\" ; exit" 192.168.2.4
+		lftp -e "mirror -R --delete /storage/media/series/\"$dir\" \"$dir\" ; exit" 192.168.2.4
 	fi
 done
