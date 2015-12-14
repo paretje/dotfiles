fc3ef2cd3aa5a55edf366704d7c1fd76118d9f30
Paretje <info@Online-Urbanus.be>
2015-12-14 11:14:22 +0100

    Workaround for fullscreen problems Iceweasel
    Recently I upgraded to Iceweasel 42, as I wanted to upgrade to a more
    recent VimFx version, but since my browser freezed when entering full
    screen. First I thought this to be a bug in Unipept, as I thought I had
    seen an error in the console, but now it seems not.
    
    I knew there was an issue with Iceweasel as I had the same problem when
    I wanted to watch a video on a website. I thought it to be an issue with
    firefox, but didn't find anything at first, until I thought of narrowing
    it down to xmonad, and indeed, I found this issue:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1189622
    
    This gave me the workaround I needed.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,17 +7,19 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import System.Exit
 import XMonad.Layout.LayoutModifier
+import XMonad.Hooks.EwmhDesktops
 
 main :: IO ()
 main = xmonad =<< myXmobar myConfig
 
-myConfig = defaultConfig
+myConfig = ewmh defaultConfig
     { modMask = mod3Mask
     , terminal = "exec urxvtcd -e sh -c 'session=$(tmux ls | grep -v -m 1 \"(attached)$\" | sed \"s/^\\([0-9]*\\):.*$/\\1/\"); if [ \"$session\" = \"\" ]; then exec tmux new-session ; else exec tmux attach-session -t $session ; fi'"
     , manageHook = myManageHook <+> manageHook defaultConfig
     , layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
     , focusFollowsMouse = False
-    , clickJustFocuses = False }
+    , clickJustFocuses = False
+    , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook }
         `additionalKeysP`
     [ ("M-S-z", spawn "xscreensaver-command --lock")
     , ("M-p", spawn "exec gmrun")
