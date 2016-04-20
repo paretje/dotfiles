23ea0265152ec770d2e7946a1255196cffa71d21
Kevin Velghe <kevin@paretje.be>
2016-04-20 14:42:48 +0200

    xmonad: more intelligent handling of layouts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,6 +8,7 @@ import XMonad.Hooks.ManageDocks
 import System.Exit
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.IfMin
 
 main :: IO ()
 main = xmonad =<< myXmobar myConfig
@@ -16,7 +17,7 @@ myConfig = defaultConfig
     { modMask = mod3Mask
     , terminal = "exec urxvtcd -e sh -c 'session=$(tmux ls | grep -v -m 1 \"(attached)$\" | sed \"s/^\\([0-9]*\\):.*$/\\1/\"); if [ \"$session\" = \"\" ]; then exec tmux new-session ; else exec tmux attach-session -t $session ; fi'"
     , manageHook = myManageHook <+> manageHook defaultConfig
-    , layoutHook = smartBorders $ avoidStruts (layoutHook defaultConfig ||| ThreeColMid 1 (3/100) (1/2))
+    , layoutHook = smartBorders $ avoidStruts $ myLayoutHook
     , focusFollowsMouse = False
     , clickJustFocuses = False }
         `additionalKeysP`
@@ -33,18 +34,21 @@ myConfig = defaultConfig
     , ("M-<F10>", spawn "amixer set Master 10%+")
     ]
 
-myManageHook :: Query (Endo WindowSet)
+myLayoutHook = ifMin 3 min3 $ ifMin 2 min2 min0 where
+    min0 = Full
+    min2 = tiled ||| Mirror tiled ||| min0
+    min3 = ThreeColMid 1 (3/100) (1/2) ||| min2
+    tiled = Tall 1 (3/100) (1/2)
+
 myManageHook = composeAll
     [ className =? "Xfrun4" --> doFloat
     , className =? "mpv" --> doFloat
     , className =? "Pinentry" --> doIgnore ]
 
 myXmobar :: LayoutClass l Window
-       => XConfig l -> IO (XConfig (ModifiedLayout AvoidStruts l))
+         => XConfig l -> IO (XConfig (ModifiedLayout AvoidStruts l))
 myXmobar = statusBar xmobarCommand xmobarPP toggleStrutsKey where
     xmobarCommand = "if [ \"$(hostname)\" = 'kevin-laptop' ]; then exec xmobar -t '" ++ lTemplate ++ "' ; else exec xmobar -t '" ++ dTemplate ++ "' ; fi"
     dTemplate = "%StdinReader% }{ %dynnetwork% | %memory% * %swap% | %cpu% %coretemp% | %default:Master%| %EBOS% | <fc=#ee9a00>%date%</fc>"
     lTemplate = "%StdinReader% }{ %3gmonitor% %wlan0wi% | %dynnetwork% | %memory% * %swap% | %cpu% %coretemp% | %battery% | %default:Master%| %EBOS% | <fc=#ee9a00>%date%</fc>"
-
-toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
-toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
+    toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
