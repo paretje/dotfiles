b3fe1b14e5b081cb232f0e4ffcd862656d99e533
Kevin Velghe <kevin@paretje.be>
2018-01-31 09:31:50 +0100

    alot: small cleanup

--- .config/alot/hooks.py
+++ .config/alot/hooks.py
@@ -4,17 +4,20 @@ import urllib2
 import subprocess
 
 
+# auto-refresh search buffer
 def pre_buffer_focus(ui, dbm, buf):
     if buf.modename == 'search':
         buf.rebuild()
 
 
+# keep track of position in search buffer
 def pre_buffer_open(ui, dbm, buf):
     current = ui.current_buffer
     if isinstance(current, alot.buffers.SearchBuffer):
         current.focused_thread = current.get_selected_thread()  # save focus
 
 
+# restore position in search buffer on return
 def post_buffer_focus(ui, dbm, buf, success):
     if success and hasattr(buf, "focused_thread"):  # if buffer has saved focus
         tid = buf.focused_thread.get_thread_id()
@@ -24,6 +27,7 @@ def post_buffer_focus(ui, dbm, buf, success):
                 break
 
 
+# mark current message read at github
 def github_mark_read(ui, msg=None):
     if msg is None:
         msg = ui.current_buffer.get_selected_message()
@@ -46,16 +50,7 @@ def github_mark_read(ui, msg=None):
         ui.notify('no beacon found')
 
 
-# def post_thread_move(ui, cmd, dbm):
-#     msg = ui.current_buffer.get_selected_message()
-#     if 'unread' in msg.get_tags() and msg.get_author()[1] == 'notifications@github.com':
-#         github_mark_read(ui, msg)
-
-#         # make sure we don't do this twice
-#         msg.remove_tags(['unread'])
-#         dbm.flush()
-
-
+# automatically mark github notifications as read
 def post_search_select(ui, cmd, dbm):
     current_msg = ui.current_buffer.get_selected_message()
     if current_msg.get_author()[1] == 'notifications@github.com':
@@ -64,6 +59,7 @@ def post_search_select(ui, cmd, dbm):
             github_mark_read(ui, list(ui.current_buffer.messagetrees())[-1]._message)
 
 
+# command to manually fetch mails
 def getmails(ui):
     ui.notify("fetchinig email..")
     subprocess.call(['getmails', '--now'])
