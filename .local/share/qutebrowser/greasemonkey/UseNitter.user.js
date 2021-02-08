9481e05ac543817b0a19f1bb4502a6cbfc3de1db
Kevin Velghe <kevin@paretje.be>
2021-02-08 13:41:30 +0100

    qutebrowser: use nitter.cc

--- .local/share/qutebrowser/greasemonkey/UseNitter.user.js
+++ .local/share/qutebrowser/greasemonkey/UseNitter.user.js
@@ -4,11 +4,12 @@
 // @description  Redirect page to Nitter
 // @author       Paretje
 // @match        *://twitter.com/*
+// @match        *://nitter.net/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    window.location.replace("https://nitter.net" + window.location.pathname + window.location.search);
+    window.location.replace("https://nitter.cc" + window.location.pathname + window.location.search);
 })();
